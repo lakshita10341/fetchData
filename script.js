10f328b6feca3ba6ab1d542edf9e5b68ff67cc1d
lakshita10341 <lakshitajainchopra89@gmail.com>
2024-04-15 00:50:50 +0530

    error handling

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
-let promise=fetch("https://reqres.in/api/users");
-promise.then((value1)=>{
-  
-    return value1.json();
-}).then((value2)=>{
-    users = value2.data;
-    console.log(users);
+try {
+    let promise = fetch("https://reqres.in/api/users");
+    promise.then((value1) => {
+        return value1.json();
+    }).then((value2) => {
+        const users = value2.data;
+        console.log(users);
 
-    const tbody = document.querySelector('#user-data tbody');
-    users.forEach(user => {
+        const tbody = document.querySelector('#user-data tbody');
+        users.forEach(user => {
+            const newRow = document.createElement('tr');
+            const id = document.createElement('td');
+            id.textContent = user.id;
 
-        
-        const newRow = document.createElement('tr');
-        const id=document.createElement('td');
-        id.textContent=user.id;
+            const email = document.createElement('td');
+            email.textContent = user.email;
 
+            const Name = document.createElement('td');
+            Name.textContent = user.first_name + " " + user.last_name;
 
-        const email=document.createElement('td');
-        email.textContent=user.email;
+            const avatar = document.createElement('td');
+            const avatarImg = document.createElement('img');
+            avatarImg.src = user.avatar;
+            avatar.appendChild(avatarImg);
 
-        const FirstName=document.createElement('td');
-        FirstName.textContent=user.first_name;
-
-        const LastName=document.createElement('td');
-        LastName.textContent=user.last_name;
-
-        const avatar=document.createElement('td');
-        const avatarImg=document.createElement('img');
-        avatarImg.src= user.avatar;
-        avatar.appendChild(avatarImg);
-       
-
-        newRow.appendChild(id);
-        newRow.appendChild(email);
-        newRow.appendChild(FirstName);
-        newRow.appendChild(LastName);
-        newRow.appendChild(avatar);
-        tbody.appendChild(newRow);
-
-
-        
+            newRow.appendChild(id);
+            newRow.appendChild(email);
+            newRow.appendChild(Name);
+            newRow.appendChild(avatar);
+            tbody.appendChild(newRow);
+        });
     });
-})
+} catch (error) {
+    console.error('Error occurred:', error);
+}
+
